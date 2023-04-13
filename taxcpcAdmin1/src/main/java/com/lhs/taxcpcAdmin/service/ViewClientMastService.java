13e0d2edd5d1e9c2e13d54163be56976bf8e40f8
VisualSVN Server <VisualSVN Server@7b5cb272-e1e2-3643-9902-95574822d9a6>
2023-04-13 11:35:14 +0000

    Deleted folder '/taxcpcAdmin1'.
    git-svn-id: https://roshanb.lispl.org/svn/JAVA-REPOSITORY@17 7b5cb272-e1e2-3643-9902-95574822d9a6

--- taxcpcAdmin1/src/main/java/com/lhs/taxcpcAdmin/service/ViewClientMastService.java
+++
@@ -1,7 +0,0 @@
-package com.lhs.taxcpcAdmin.service;
-
-import com.lhs.taxcpcAdmin.model.entity.ViewClientMast;
-
-public interface ViewClientMastService {
-	ViewClientMast getClientById(String client_code);
-}
